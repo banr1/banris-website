d31b5c0b12e80831e417d66f8cee79248fee3b4c
banri <banri.yanahama@gmail.com>
2024-10-02 19:36:01 +0900

    update notion css

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Moon, Sun } from 'lucide-react';
 
+// This component is not used for now.
 const DarkModeToggle = () => {
   const [darkMode, setDarkMode] = useState(false);
 
